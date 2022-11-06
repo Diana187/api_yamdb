fba2ef2cc044f2ed26bf28989131a34bf1992d7e
Диана Амирханова <dinaami187@gmail.com>
2022-11-06 15:01:24 +0000

    serializers.py add TokenSerializer

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,15 +1,31 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-
+from rest_framework.validators import UniqueTogetherValidator, ValidationError
 
 from reviews.models import Title, Category
+from users.models import User
 
 
-class GetTokenSerializer():
-    pass
+class GetTokenSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        required=True)
+    confirmation_code = serializers.CharField(
+        required=True)
+
+    class Meta:
+        fields = '__all__'
+        model = User
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=User.objects.all(),
+                fields=['user', 'confirmation_code']
+            )
+        ]
 
 
 class SignUpSerializer():
-    pass
+    
 
 
 class UserSerializer():
