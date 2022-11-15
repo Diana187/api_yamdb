804a4108591752eb81996965ff6a45952fac7f90
Андрей Черняев <47273147+ChernyaevAV@users.noreply.github.com>
2022-11-15 06:59:25 +0300

    Update mixins.py

--- api_yamdb/api/mixins.py
+++ api_yamdb/api/mixins.py
@@ -1,3 +1,12 @@
+from rest_framework.mixins import (
+    CreateModelMixin,
+    ListModelMixin,
+    DestroyModelMixin,
+)
+
+from rest_framework import viewsets
+
+
 class CreateListDestroyViewSet(
     CreateModelMixin,
     ListModelMixin,
